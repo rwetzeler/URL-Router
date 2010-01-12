36930863c78d635b513112cc14a2eada735fa23f
Rob Wetzeler <rob.wetzeler@fcstone.com>
2010-01-12 10:57:22 -0600

    added cleanOnly and cleanFirst arg options so that it can either just clean (delete) based on the config or clean first and then add

--- src/ReRouter/ReRouter/Program.cs
+++ src/ReRouter/ReRouter/Program.cs
@@ -6,22 +6,58 @@ namespace ReRouter
     using Config;
     using System;
     using System.Diagnostics;
+    using System.Collections.Generic;
 
     public class Program
     {
         static void Main(string[] args)
         {
+            var listArgs = new List<string>(args);
             var configSection = (UrlRouteConfigSection)ConfigurationManager.GetSection(
                 ConfigurationManager.AppSettings["ReRouterConfigSection"]);
 
             if (configSection == null)
                 throw new NullReferenceException("Could not find configSection by the name: " + ConfigurationManager.AppSettings["ReRouterConfigSection"]);
 
+            if (listArgs.Contains("cleanFirst") || listArgs.Contains("cleanOnly"))
+            {
+                DeleteConfiguredRoutesBeforeAdding(configSection.Routes);
+            }
+
+            if (listArgs.Contains("cleanOnly"))
+            {
+                return;
+            }
+
             foreach (UrlRouteElement route in configSection.Routes)
             {
                 AddRoute(route, configSection.OverrideGatewayIp);
             }
         }
+
+        private static void DeleteConfiguredRoutesBeforeAdding(UrlRouteElementCollection routes)
+        {
+            foreach(UrlRouteElement route in routes)
+            {
+                DeleteRount(route);
+            }
+        }
+
+        private static void DeleteRount(UrlRouteElement route)
+        {
+            string ipAddress = route.IpAddress;
+
+            if (string.IsNullOrEmpty(ipAddress))
+            {
+                ipAddress = FindIpAddress(route.Url);
+            }
+
+            var proc = new Process();
+            proc.StartInfo.FileName = "route";
+            proc.StartInfo.Arguments = string.Format("delete {0}", ipAddress);
+            proc.Start();
+        }
+
 
         private static void AddRoute(UrlRouteElement route, string gateway)
         {
@@ -40,7 +76,6 @@ namespace ReRouter
 
         private static string FindIpAddress(string routeUrl)
         {
-
             IPHostEntry ip = Dns.GetHostByName(routeUrl);
             if(ip == null)
                 throw new NullReferenceException("Could not find host using: " + routeUrl);
